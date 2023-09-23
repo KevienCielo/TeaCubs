d33cdc5581a01e2098034d492566fb32f7cd404c
Mark Cielo <cielomarkkevien@gmail.com>
2023-09-24 04:19:13 +0800

    added user login & registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import "./bootstrap";
+
+// Start of Show/Hide Password
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type =
+        password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});
+// End of Show/Hide Password
